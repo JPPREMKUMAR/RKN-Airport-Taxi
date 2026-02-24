1624d2190fb053ffac8b8d96700e369f4889168e
JPPREMKUMAR <jettiprasangipremkumar012@gmail.com>
2026-02-24 15:59:31 +0530

    new ww

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,13 @@ import connectCloudinary from "./config/connectCloudinary.js"
 dotenv.config()
 
 const app = express()
-// Middleware 
 app.use(express.json())
 
 
 
 app.use(cors({
-    origin: "*", // allow all browsers
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+
 }));
 
 
