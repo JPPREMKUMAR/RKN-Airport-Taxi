4409c7fec9c1476c28a7568308909cede46d408d
JPPREMKUMAR <jettiprasangipremkumar012@gmail.com>
2026-03-11 21:11:27 +0530

    new update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,11 @@ app.use(express.json())
 
 
 
-app.use(cors());
+app.use(cors({
+    origin: "*", // allow all browsers
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 
 
